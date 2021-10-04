2e6782f30d1ca47516ceb46c71c3c4fd2355b56b
rolandomunoz <rolando.muar@gmail.com>
2021-10-04 16:46:29 -0500

    fix: import path

--- src/mytextgrid/__init__.py
+++ src/mytextgrid/__init__.py
@@ -1,4 +1,4 @@
 """"Import only functions
 """
-from mytextgrid.textgrid import create_textgrid
+from mytextgrid.textgrid.textgrid import create_textgrid
 from mytextgrid.parser.full_text_format import read_from_file
