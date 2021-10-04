0312c75044cdeb2267a410b5cb16743efe1d3429
rolandomunoz <rolando.muar@gmail.com>
2021-10-04 16:35:04 -0500

    style: use absolute paths for importing

--- src/mytextgrid/__init__.py
+++ src/mytextgrid/__init__.py
@@ -1,2 +1,4 @@
-from .textgrid import create_textgrid
-from .parser.full_text_format import read_from_file
+""""Import only functions
+"""
+from mytextgrid.textgrid import create_textgrid
+from mytextgrid.parser.full_text_format import read_from_file
