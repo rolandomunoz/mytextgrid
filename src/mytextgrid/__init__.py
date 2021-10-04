b6531b5b3f80e2bfb06d0c1ea39d993f7835b4c2
rolandomunoz <rolando.muar@gmail.com>
2021-10-04 16:55:13 -0500

    docs: add documentation

--- src/mytextgrid/__init__.py
+++ src/mytextgrid/__init__.py
@@ -1,4 +1,5 @@
-""""Import only functions
+"""A tiny python project for creating, reading, writing and querying Praat annotation files
+(TextGrid).
 """
 from mytextgrid.textgrid.textgrid import create_textgrid
 from mytextgrid.parser.full_text_format import read_from_file
