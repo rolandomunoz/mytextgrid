72c38b2ae6dc3846984cb761469b479656e195c1
rolandomunoz <rolando.muar@gmail.com>
2021-12-19 11:29:22 -0500

    doc: Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Rolando Muñoz Aramburú'
 author = 'Rolando Muñoz Aramburú'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.2.0'
+release = 'v0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
