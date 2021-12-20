a604298ae9c6e752321cf3bdbeaf7990ec864b56
rolandomunoz <rolando.muar@gmail.com>
2021-12-19 21:18:57 -0500

    doc: Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/mytextgrid/'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 
 # -- Project information -----------------------------------------------------
 
