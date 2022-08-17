7a9ae80643d7274914befe1735fcd6291319afc4
rolandomunoz <rolando.muar@gmail.com>
2022-08-16 22:25:09 -0500

    feat: use property decorator

--- src/mytextgrid/core/tier.py
+++ src/mytextgrid/core/tier.py
@@ -4,24 +4,69 @@ from mytextgrid._eval import EvalTimeRange
 decimal.getcontext().prec = 16
 
 class Tier:
-    """Tier is the base class for IntervalTier and PointTier classes. It represents an item's container."""
-
+    """
+    This is a base class for :class:`IntervalTier` and :class:`PointTier classes`. It represents an item's container.
+    """
     def __init__(self, name = '', xmin = 0, xmax = 1, is_interval = True):
-        self.name = name
+        self._name = name
         self._is_interval = is_interval
-        self.xmin = decimal.Decimal(str(xmin))
-        self.xmax = decimal.Decimal(str(xmax))
-        self.items = []
-        self.eval_time_range = EvalTimeRange(self.xmin, self.xmax, level = 1)
+        self._xmin = decimal.Decimal(str(xmin))
+        self._xmax = decimal.Decimal(str(xmax))
+        self._items = []
+        self.eval_time_range = EvalTimeRange(self._xmin, self._xmax, level = 1)
 
     def __len__(self):
-        return len(self.items)
+        return len(self._items)
 
     def __iter__(self):
-        return iter(self.items)
+        return iter(self._items)
 
     def __getitem__(self, key):
-        return self.items[key]
+        return self._items[key]
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def is_interval(self):
+        return self._is_interval
+
+    @property
+    def xmin(self):
+        return self._xmin
+
+    @xmin.setter
+    def xmin(self, value):
+        xmin = decimal.Decimal(str(value))
+
+        if xmin > self._xmax:
+            raise ValueError('xmin must be lesser than xmax.')
+
+        # Change the object time
+            self._xmin = xmin
+
+    @property
+    def xmax(self):
+        return self._xmax
+
+    @xmax.setter
+    def xmax(self, value):
+        xmax = decimal.Decimal(str(value))
+
+        if xmax < self._xmin:
+            raise ValueError('xmax must be greater than xmin.')
+
+        # Change the object time
+            self._xmax = xmax
+
+    @property
+    def items(self):
+        return self._items
+
+    @items.setter
+    def items(self, list_):
+        self.items_ = list_
 
     def get_duration(self):
         """
