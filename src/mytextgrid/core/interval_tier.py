a841f2e3e7434d5337d10d91a18fe7f70d1474be
rolandomunoz <rolando.muar@gmail.com>
2022-09-06 17:55:10 -0500

    refactor: Update methods

--- src/mytextgrid/core/interval_tier.py
+++ src/mytextgrid/core/interval_tier.py
@@ -1,16 +1,28 @@
 """Create and manipulate `Interval` and `IntervalTier` objects."""
 import decimal
 from mytextgrid.core.tier import Tier
+from mytextgrid.eval import obj_to_decimal
 decimal.getcontext().prec = 16
 
 class IntervalTier(Tier):
     """
     A class representation for a interval tier.
     """
-
     def __init__(self, name = '', xmin = 0, xmax = 1):
+        """
+        Initialize an instance of IntervalTier.
+
+        Parameters
+        ----------
+        name : str, default ''
+            The name of the tier.
+        xmin : int, float str or decimal.Decimal
+            The starting time (in seconds) of the tier.
+        xmin : int, float str or decimal.Decimal
+            The ending time (in seconds) of the tier.
+        """
         super().__init__(name, xmin, xmax, is_interval = True)
-        self.items = [Interval(self.xmin, self.xmax)]
+        self._items = [Interval(self._xmin, self._xmax)]
 
     def insert_boundaries(self, *times):
         """
@@ -30,7 +42,7 @@ class IntervalTier(Tier):
 
         Parameters
         ----------
-        time : float or decimal.Decimal
+        time : int, float, str or :clas::`decimal.Decimal`
             The time (in seconds) of the new boundary.
 
         Returns
@@ -41,30 +53,33 @@ class IntervalTier(Tier):
         Raises
         ------
         ValueError
-            If the the specified time already exists.
+            If the specified time already exists.
         """
-        if not isinstance(time, decimal.Decimal):
-            time = decimal.Decimal(str(time))
+        time_ = obj_to_decimal(time)
+        self.eval_time_range(time_) # Raise exceptions if out of the tier range
 
-        # Check if out of range
-        self.eval_time_range.check_time(time)
+        index = self.get_index_at_time(time_)
+        old_interval = self._items[index]
 
-        loc = self.get_interval_at_time(time)
-        interval_left = self.items[loc]
-        if time == interval_left.xmin:
-            error_msg = (f'Cannot add a boundary at {time} seconds,'
-            'because there is already a boundary there. Boundary not inserted.')
-            raise ValueError(error_msg)
+        if old_interval.xmin == time_:
+            raise ValueError(f'There is already a boundary at {time_}')
 
-        # Split interval
-        interval_right = Interval(time, interval_left.xmax)
-        interval_left.xmax= time
+        new_left_interval = Interval(
+            xmin = old_interval.xmin,
+            xmax = time_
+        )
 
-        # Insert interval
-        self.items.insert(loc+1, interval_right)
+        new_right_interval = Interval(
+            xmin = time_,
+            xmax = old_interval.xmax
+        )
+
+        # Update items
+        self._items[index] = new_left_interval
+        self._items.insert(index+1, new_right_interval)
 
         # Returns
-        return (loc, loc+1)
+        return (index, index+1)
 
     def remove_boundary(self, time):
         """
@@ -72,33 +87,34 @@ class IntervalTier(Tier):
 
         Parameters
         ----------
-        time : float or decimal.Decimal
-            The time (in seconds) of an existing boundary.
+        time : int, float, str or :clas::`decimal.Decimal`
+            The time (in seconds) of the new boundary.
 
         Raises
         ------
         ValueError
             If there is there is not a boundary at the specified time.
         """
-        if not isinstance(time, decimal.Decimal):
-            time = decimal.Decimal(str(time))
+        time_ = obj_to_decimal(time)
+        self.eval_time_range(time_) # Raise exceptions if out of range
 
-        self.eval_time_range.check_time(time)
+        index = self.get_index_at_time_boundary(time_)
+        if index is None:
+            raise ValueError(f'No boundary to remove at {time}.')
 
-        loc = self.get_interval_at_time(time)
-        interval_left = self.items[loc]
-        if not time == interval_left.xmin:
-            error_msg = f'There is no boundary at {time} seconds'
-            raise ValueError(error_msg)
+        interval_left = self._items[index-1]
+        interval_right = self._items[index]
 
-        interval_left = self.items[loc-1]
-        interval_right = self.items[loc]
+        # Create and insert new interval
+        new_interval  = Interval(
+            xmin = interval_left.xmin,
+            xmax = interval_right.xmax,
+            text = interval_left.text + interval_right.text
+        )
 
-        interval_left.xmax = interval_right.xmax
-        interval_left.text += interval_right.text
-
-        # Remove right interval
-        self.items.pop(loc)
+        # Update items
+        self._items[index] = new_interval
+        self._items.remove(interval_left)
 
     def move_boundary(self, src_time, dst_time):
         """
@@ -118,139 +134,211 @@ class IntervalTier(Tier):
             If the new time location is outside of the time range of the left and right intervals.
         """
         # Normalize numbers
-        if not isinstance(src_time, decimal.Decimal):
-            src_time = decimal.Decimal(str(src_time))
+        src_time_ = obj_to_decimal(src_time)
+        dst_time_ = obj_to_decimal(dst_time)
 
-        if not isinstance(dst_time, decimal.Decimal):
-            dst_time = decimal.Decimal(str(dst_time))
+        # Raise exceptions.
+        if src_time_ == dst_time_:
+            raise ValueError('src_time and dst_time MUST NOT BE equal')
+        self.eval_time_range(src_time_)
+        self.eval_time_range(dst_time_)
 
-        # Check
-        self.eval_time_range.check_time(src_time)
-        self.eval_time_range.check_time(dst_time)
+        ## Get/Check if src boundary time exists.
+        index = self.get_index_at_time_boundary(src_time_)
+        if index is None:
+            raise ValueError(f'No boundary found at src_time ({src_time_})')
 
-        position = self.get_interval_at_time(src_time)
+        left_interval = self._items[index-1]
+        right_interval = self._items[index]
 
-        interval_left = self.items[position-1]
-        interval_right = self.items[position]
+        ## Check if dst_time is outside its neighbors boundaries.
+        if left_interval.xmin >= dst_time_ >= right_interval.xmax:
+            raise ValueError('Cannot move the source boundary outside its neighbors boundaries.')
 
-        if not interval_right.xmin == src_time:
-            raise ValueError(f'There is no boundary at {src_time} seconds')
+        # Create intervals objects and replace them.
+        new_left_interval = Interval(
+            xmin = left_interval.xmin,
+            xmax = dst_time_,
+            text = left_interval.text
+        )
 
-        if not dst_time > interval_left.xmin or not dst_time < interval_right.xmax:
-            raise ValueError(f'Cannot move boundary to {dst_time}')
+        new_right_interval = Interval(
+            xmin = dst_time_,
+            xmax = right_interval.xmax,
+            text = right_interval.text
+        )
 
-        # Do
-        interval_left.xmax = dst_time
-        interval_right.xmin = dst_time
+        # Update items
+        self._items[index-1] = new_left_interval
+        self._items[index] = new_right_interval
 
-    def set_text(self, position, *text_items):
+    def set_text_at_index(self, index, *text_items):
         """
         Set the text of one or more of intervals.
 
         If more than one text item is provided, intervals will be set from left to right
-        counting from the starting `position`.
+        counting from the starting `index`.
 
         Parameters
         ----------
-        position : int
-            The start position where text items will be inserted in the ``position``.
-        *text_items
+        index : int
+            The index at which text items will be inserted.
+        text_items : str or iterable of str
             The text items that will be inserted.
         """
-        for loc, text in enumerate(text_items, start = position):
-            self.items[loc].text = text
+        # Raise Exception if more text items than intervals.
+        if (index + len(text_items) - 1) > len(self._items):
+            raise IndexError('more text items than intervals.')
 
-    def get_interval_at_time(self, time):
+        for index_, text in enumerate(text_items, start = index):
+            self._items[index_].text = text
+
+    def get_index_at_time(self, time):
         """
-        Search the IntervalTier for an interval position at the specified time.
+        Return the index of an interval at the given time.
 
-        This method matches the interval where the specified time is greater than
-        or equal to its left boundary or less than its right boundary.
+        If time relies on the boundary between two intervals, it returns
+        the index of the right interval.
 
         Parameters
         ----------
-        time : float or decimal.Decimal
+        time : int, float, str or :class:`decimal.Decimal`
             The time in seconds to be evaluated.
 
         Returns
         -------
-        int
-            Return the interval position of the specified time.
+        int or None
+            Return the interval index at the specified time.
         """
-        self.eval_time_range.check_time(time)
-        if not isinstance(time, decimal.Decimal):
-            time = decimal.Decimal(str(time))
-        for position, interval in enumerate(self):
-            if interval.is_in_range(time):
-                return position
-        self.xmin <= time < self.xmax
+        time_ = obj_to_decimal(time)
+
+        if time_ == self._xmax:
+            return len(self._items) - 1 # Returns the last index
+
+        for index, interval in enumerate(self._items):
+            if interval.xmin <= time_ < interval.xmax:
+                return index
+        return None
+
+    def get_interval_at_time(self, time):
+        """
+        Return the interval at the specified time.
+
+        If `time` relies on the boundary between two intervals, it returns
+        the the right interval.
+
+        Parameters
+        ----------
+        time : int, float, str or :class:`decimal.Decimal`
+            The time in seconds to be evaluated.
+
+        Returns
+        -------
+        :class:`mytextgrid.core.interval_tier.Interval` or None
+            Return the interval index at the specified time.
+        """
+        index = self.get_index_at_time(time)
+        if index is None:
+            return None
+        return self._items[index]
+
+    def get_index_at_time_boundary(self, time):
+        """
+        Get the index of an interval at a given boundary.
+
+        Only counts the inserted boundaries.
+
+        Parameters
+        ----------
+        time : int, float, str or :class:`decimal.Decimal`
+            The time to match with any boundary.
+
+        Returns
+        -------
+        int or None
+            Return an integer if the
+        """
+        time_ = obj_to_decimal(time)
+
+        for index, interval in enumerate(self._items):
+            if index == 0:
+                continue
+            if interval.xmin == time_:
+                return index
         return None
 
 class Interval:
     """
     A class representation for an interval.
     """
-
-    def __init__(self, xmin, xmax, text = ""):
-        self.class_ = 'Interval'
-        self.xmin = decimal.Decimal(str(xmin))
-        self.xmax = decimal.Decimal(str(xmax))
-        self.text = text
-
-    def is_in_range(self, time):
+    def __init__(self, xmin, xmax, text = ''):
         """
-        Evaluate if the specified time is between the interval boundaries.
+        Init an object representing a Praat interval.
 
         Parameters
         ----------
-        time : float or decimal.Decimal
-            The time in seconds to be evaluated.
-
-        Returns
-        -------
-        bool
-            True is the specified tier is Interval range, that is (xmin >= time < xmax).
-            Otherwise, False.
+        xmin : int, float str or decimal.Decimal
+            The starting time (in seconds) of the interval.
+        xmin : int, float str or decimal.Decimal
+            The ending time (in seconds) of the interval.
+        text : str, default ''
+            The text content of the interval.
         """
-        time = decimal.Decimal(str(time))
-        if self.xmin <= time < self.xmax:
-            return True
-        return False
+        # Check input type
+        if not isinstance(text, str):
+            raise TypeError('text MUST BE a str.')
+        if not isinstance(xmin, (int, float, str, decimal.Decimal)):
+            raise TypeError('xmin MUST BE an int, float, str or decimal.Decimal.')
+        if not isinstance(xmax, (int, float, str, decimal.Decimal)):
+            raise TypeError('xmax MUST BE an int, float, str or decimal.Decimal.')
 
-    def is_left_boundary(self, time):
+        xmin_ = obj_to_decimal(xmin)
+        xmax_ = obj_to_decimal(xmax)
+
+        assert xmax_ > xmin_, 'xmax MUST BE greater than xmin'
+
+        # Assign attributes
+        self._xmin = xmin_
+        self._xmax = xmax_
+        self._text = text
+
+    @property
+    def xmin(self):
         """
-        Evaluate if the specified time is equal to the left boundary time.
-
-        Parameters
-        ----------
-        time : float or decimal.Decimal
-            The time in seconds to be evaluated.
-
-        Returns
-        -------
-        bool
-            True is the specified tier is equal to the left boundary. Otherwise, False.
+        Return _xmin attribute.
         """
-        time = decimal.Decimal(str(time))
-        if self.xmin == time:
-            return True
-        return False
+        return self._xmin
 
-    def is_right_boundary(self, time):
+    @property
+    def xmax(self):
         """
-        Evaluate if the specified time is equal to the right boundary time.
-
-        Parameters
-        ----------
-        time : float or decimal.Decimal
-            The time in seconds to be evaluated.
-
-        Returns
-        -------
-        bool
-            True is the specified tier is equal to the right boundary. Otherwise, False.
+        Return _xmax attribute.
         """
-        time = decimal.Decimal(str(time))
-        if self.xmax == time:
-            return True
-        return False
+        return self._xmax
+
+    @property
+    def text(self):
+        """
+        Return _text attribute.
+        """
+        return self._text
+
+    @text.setter
+    def text(self, value):
+        """
+        Modify _text attribute.
+        """
+        if not isinstance(value, str):
+            raise TypeError('text MUST BE a str')
+        self._text = value
+
+    def get_duration(self):
+        """
+        Return the duration of the interval.
+
+        Return
+        ------
+        :class:`decimal.Decimal`
+            The duration of the interval.
+        """
+        return self._xmax - self._xmin
