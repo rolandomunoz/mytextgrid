2615d4f3c66676a41dbf1b16a5f5a50fb63f3d21
rolandomunoz <rolando.muar@gmail.com>
2022-09-25 08:11:08 -0500

    - add: Convert TextGrid to dictionary - add: new to_textgrid2() method. This will use a template. - add: new package jinja2

--- src/mytextgrid/io/textgrid_out.py
+++ src/mytextgrid/io/textgrid_out.py
@@ -1,6 +1,13 @@
-"""Export TextGrid files to other formats"""
+"""
+Export TextGrid files to other formats
+"""
 import csv
 import json
+import jinja2
+from pathlib import Path
+
+def to_textgrid2(textgrid_obj, dst_path, encoding = 'utf-8'):
+    template_path = Path(__file__) / 'templates/long_textgrid_format.TextGrid.jinja'
 
 def to_textgrid(textgrid_obj, path, encoding = 'utf-8'):
     """
@@ -95,6 +102,45 @@ def to_csv(textgrid_obj, path, encoding = 'utf-8'):
         for row in table:
             spamwriter.writerow([str(item) for item in row])
 
+def to_dict(textgrid_obj):
+    """
+
+    """
+    textgrid = {
+        'xmin': textgrid_obj.xmin,
+        'xmax': textgrid_obj.xmax,
+        'tiers': []
+        }
+    for tier in textgrid_obj:
+        tier_class = 'IntervalTier' if tier.is_interval() else 'TextTier'
+
+        textgrid['tiers'].append(
+            {
+            'class':tier_class,
+            'name':tier.name,
+            'items':[]
+            }
+            )
+
+        for item in tier:
+            if tier.is_interval():
+                # If IntervalTier
+                textgrid['tiers'][-1]['items'].append(
+                {
+                'xmin':str(item.xmin),
+                'xmax':str(item.xmax),
+                'text':item.text
+                }
+                )
+            else:
+                # If PointTier
+                textgrid['tiers'][-1]['items'].append(
+                {
+                'number':str(item.time),
+                'mark':item.text
+                }
+                )
+
 def to_json(textgrid_obj, path, encoding = 'utf-8'):
     """
     Write TextGrid to a json file.
@@ -143,5 +189,5 @@ def to_json(textgrid_obj, path, encoding = 'utf-8'):
                 }
                 )
 
-    with open(path, 'w', encoding = encoding) as file_object:
-        json.dump(textgrid, file_object, ensure_ascii = False, indent = 4)
+        with open(path, 'w', encoding = encoding) as file_object:
+            json.dump(textgrid, file_object, ensure_ascii = False, indent = 4)
