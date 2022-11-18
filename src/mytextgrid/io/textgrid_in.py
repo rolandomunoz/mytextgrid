60106c05b9c62367eff7ff49903c423265c1c874
rolandomunoz <rolando.muar@gmail.com>
2022-11-17 23:20:34 -0500

    add: read_from_stream()

--- src/mytextgrid/io/textgrid_in.py
+++ src/mytextgrid/io/textgrid_in.py
@@ -1,4 +1,4 @@
-from mytextgrid.io.parser import full_text_format
+from mytextgrid.io.parser import fulltext_format
 
 def read_from_file(path, encoding = None):
     """
@@ -6,17 +6,40 @@ def read_from_file(path, encoding = None):
 
     Parameters
     ----------
-        path : str
-            The path of the TextGrid file.
-        encoding : str, default None, detect automatically the encoding.
-            The name of the encoding used to decode the file. See the codecs module for the list
-            supported encodings.
+    path : str
+        The path of the TextGrid file.
+    encoding : str, default None, detect automatically the encoding.
+        The name of the encoding used to decode the file. See the codecs module for the list
+        supported encodings.
 
     Returns
     -------
         :class:`mytextgrid.TextGrid`
             A TextGrid instance.
     """
-    textgrid_dict = full_text_format.textgrid_to_dict(path, encoding)
-    textgrid_obj = full_text_format.dict_to_textgrid(textgrid_dict)
+    textgrid_dict = fulltext_format.parse_textgrid_file(path, encoding)
+    textgrid_obj = fulltext_format.dict_to_textgrid(textgrid_dict)
+    return textgrid_obj
+
+def read_from_stream(stram, name, path):
+    """
+    Read a stream into a TextGrid object.
+
+    Parameters
+    ----------
+    stream : str or :class:`io.StringIO`
+        The content of a full-formatted TextGrid.
+    name : str
+        The name of the TextGrid.
+    path : str or :class:`pathlib.Path`
+        The path of the TextGrid.
+
+    Returns
+    -------
+        :class:`mytextgrid.TextGrid`
+            A TextGrid instance.
+            A TextGrid instance.
+    """
+    textgrid_dict = fulltext_format.parse(*args, **kwds)
+    textgrid_obj = fulltext_format.dict_to_textgrid(textgrid_dict)
     return textgrid_obj
