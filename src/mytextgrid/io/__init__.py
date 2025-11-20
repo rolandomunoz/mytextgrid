23fd5379db090a4013b725f2f272bd7b1d9fefd9
rolandomunoz <rolando.muar@gmail.com>
2025-11-19 20:21:10 -0500

    update public interface

--- src/mytextgrid/io/__init__.py
+++ src/mytextgrid/io/__init__.py
@@ -1,7 +1,3 @@
-from textgrid import create_textgrid
-
-from mytextgrid.io.writer import (
-    write_textgrid,
-    write_json,
-)
+from mytextgrid.io.textgrid import create_textgrid
 from mytextgrid.io.reader import read_textgrid
+from mytextgrid.io.reader import read_textgrid_from_stream
