62d537b2595169343d0bc22421b3837aaf0f59e0
rolandomunoz <rolando.muar@gmail.com>
2025-11-23 15:10:21 -0500

    edit: make public is_textgrid_file function

--- src/mytextgrid/__init__.py
+++ src/mytextgrid/__init__.py
@@ -1,3 +1,4 @@
 from mytextgrid.io import create_textgrid
 from mytextgrid.io import read_textgrid
 from mytextgrid.io import read_textgrid_from_stream
+from mytextgrid.io.utils import is_textgrid_file
