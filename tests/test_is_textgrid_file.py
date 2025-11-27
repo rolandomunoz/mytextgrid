f0d33851f852e1fd304d1ee16d41c6f0e469b902
rolandomunoz <rolando.muar@gmail.com>
2025-11-27 04:39:21 -0500

    test: include a test for binary textgrids only

--- tests/test_is_textgrid_file.py
+++ tests/test_is_textgrid_file.py
@@ -38,12 +38,32 @@ class Testpoint(unittest.TestCase):
             ("text-UTF8-SHORT-CRLF.TextGrid", True),
             ("text-UTF8-SHORT-LF.TextGrid", True),
         ]
+        self.bin_fnames = [
+            "text-UTF16-BIN.TextGrid",
+            "text-UTF8-BIN.TextGrid",
+        ]
 
     def test_textgrid_header_validator(self):
         for fname, is_textgrid in self.fnames:
             path = self.src_dir / fname
-            is_textgrid_file = utils.is_textgrid_file(path)
+            is_textgrid_file = utils.is_textgrid_file(path, True)
             self.assertEqual(is_textgrid_file, is_textgrid)
+
+    def test_binary_textgrid_header_validator(self):
+        for fname in self.bin_fnames:
+            path = self.src_dir / fname
+
+            # Inclue binary files
+            is_textgrid = utils.is_textgrid_file(path,
+                                                 include_binary=True)
+            self.assertEqual(is_textgrid, True)
+
+            # Do not inclue binary files
+            is_textgrid = utils.is_textgrid_file(path,
+                                                 include_binary=False)
+            self.assertEqual(is_textgrid, False)
+
+
 
 if __name__ == '__main__':
     unittest.main()
